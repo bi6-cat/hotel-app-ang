15cb51301d25a6a7d57ca46f61ff3f68e9e3d7d6
Nguyen Quang <quangnn906@gmail.com>
2024-11-27 16:38:21 +0700

    Step 2: Create CustomerLayout, ManagerLayout and footer, header, sidebar component.

--- src/modules/app.routes.ts
+++ src/modules/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ManagerLayoutComponent } from './shared/layouts/manager-layout/manager-layout.component';
+import { CustomerLayoutComponent } from './shared/layouts/customer-layout/customer-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'manager',
+    component: ManagerLayoutComponent,
+  },
+  {
+    path: '',
+    component: CustomerLayoutComponent,
+  },
+];
